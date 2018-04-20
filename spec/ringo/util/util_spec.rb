a1e419d8c65d59c8387ca9e76e519705ddadc5f5
David Lains <dlains@gmail.com>
2018-04-20 16:08:59 -0700

    Shuffling some code around.
    Added some answers to chapter one exercises. Added ‘car’ and ‘cdr’ to make reading the code more similar to the source. I’ll probably add some of the other Scheme methods like ‘cons’.

---
+++ spec/ringo/util/util_spec.rb
@@ -0,0 +1,155 @@
+require 'spec_helper'
+
+RSpec.describe Ringo::Util do
+  describe 'length' do
+    it 'returns zero if the list is nil' do
+      expect(Ringo::Util.length(nil)).to eq(0)
+    end
+
+    it 'returns zero if the list is empty' do
+      expect(Ringo::Util.length([])).to eq(0)
+    end
+
+    it 'returns the correct size' do
+      expect(Ringo::Util.length([1, 2, 3])).to eq(3)
+    end
+
+    it 'returns the correct size in the presence of sub lists' do
+      expect(Ringo::Util.length([1, [2, 3], 3, 4])).to eq(4)
+    end
+  end
+
+  describe 'element_at' do
+    it 'raises an error if the list is empty' do
+      expect{Ringo::Util.element_at([], 1)}.to raise_error(ArgumentError)
+    end
+
+    it 'raises an error if the list is nil' do
+      expect{Ringo::Util.element_at(nil, 1)}.to raise_error(ArgumentError)
+    end
+
+    it 'returns the correct element' do
+      expect(Ringo::Util.element_at([1, 2], 1)).to eq(2)
+    end
+  end
+
+  describe 'duple' do
+    it 'returns an empty list if count is zero' do
+      expect(Ringo::Util.duple(0, 'a')).to eq([])
+    end
+
+    it 'returns a single element list if the count is one' do
+      expect(Ringo::Util.duple(1, 'a')).to eq(['a'])
+    end
+
+    it 'returns a two element list if the count is two' do
+      expect(Ringo::Util.duple(2, 'a')).to eq(['a','a'])
+    end
+
+    it 'can handle lists as the value' do
+      expect(Ringo::Util.duple(2, [1, 2])).to eq([[1, 2], [1, 2]])
+    end
+  end
+
+  describe 'down' do
+    it 'raises an error if the list is nil' do
+      expect{Ringo::Util.down(nil)}.to raise_error(ArgumentError)
+    end
+
+    it 'raises an error if the argument is not a list' do
+      expect{Ringo::Util.down(1)}.to raise_error(ArgumentError)
+    end
+
+    it 'raises an error if the argument is empty' do
+      expect{Ringo::Util.down([])}.to raise_error(ArgumentError)
+    end
+
+    it 'wraps a single element list correctly' do
+      expect(Ringo::Util.down([1])).to eq([[1]])
+    end
+
+    it 'wraps a multiple element list correctly' do
+      expect(Ringo::Util.down([1, 2, 3])).to eq([[1],[2],[3]])
+    end
+
+    it 'wraps a multiple element list with sub lists correctly' do
+      expect(Ringo::Util.down([1, [2, 3], 4])).to eq([[1], [[2,3]], [4]])
+    end
+  end
+
+  describe 'swapper' do
+    it 'returns nil if the list is nil' do
+      expect(Ringo::Util.swapper(1, 2, nil)).to be_nil
+    end
+
+    it 'returns an empty list if the argument is empty' do
+      expect(Ringo::Util.swapper(1, 2, [])).to eq([])
+    end
+
+    it 'returns an list with the correct elements swapped' do
+      expect(Ringo::Util.swapper(1, 2, [1, 2])).to eq([2, 1])
+    end
+
+    it 'correctly swaps elements in sub lists' do
+      expect(Ringo::Util.swapper(1, 2, [1, 2, [3, 4, 1], [2]])).to eq([2, 1, [3, 4, 2], [1]])
+    end
+  end
+
+  describe 'filter_in' do
+    it 'returns nil if the list is nil' do
+      expect(Ringo::Util.filter_in(nil, -> (elem) { elem.is_a?(Numeric) })).to be_nil
+    end
+
+    it 'returns an empty list if the argument is empty' do
+      expect(Ringo::Util.filter_in([], -> (elem) { elem.is_a?(Numeric) })).to eq([])
+    end
+
+    it 'returns a filtered list of numbers' do
+      expect(Ringo::Util.filter_in(['a', 2, 'b', 4], -> (elem) { elem.is_a?(Numeric) })).to eq([2, 4])
+    end
+  end
+
+  describe 'car' do
+    it 'raises an error if the list is empty' do
+      expect{Ringo::Util.car([])}.to raise_error(ArgumentError)
+    end
+
+    it 'raises an error if the argument is not a list' do
+      expect{Ringo::Util.car(1)}.to raise_error(ArgumentError)
+    end
+
+    it 'returns the first character' do
+      expect(Ringo::Util.car(['a','b','c'])).to eq('a')
+    end
+
+    it 'returns the first list' do
+      expect(Ringo::Util.car([[1, 6], 2, 3])).to eq([1,6])
+    end
+
+    it 'returns the correct element when nested' do
+      expect(Ringo::Util.car(Ringo::Util.car([[[3]], [4]]))).to eq([3])
+    end
+  end
+
+  describe 'cdr' do
+    it 'raises an error if the list is empty' do
+      expect{Ringo::Util.cdr([])}.to raise_error(ArgumentError)
+    end
+
+    it 'raises an error if the argument is not a list' do
+      expect{Ringo::Util.cdr(1)}.to raise_error(ArgumentError)
+    end
+
+    it 'returns the remaining characters' do
+      expect(Ringo::Util.cdr(['a','b','c'])).to eq(['b','c'])
+    end
+
+    it 'returns the remaining list' do
+      expect(Ringo::Util.cdr([[1, 6], [2], 3])).to eq([[2], 3])
+    end
+
+    it 'returns the correct element when nested' do
+      expect(Ringo::Util.cdr(Ringo::Util.cdr([[1],[7,8],[[2]]]))).to eq([[[2]]])
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 require 'bundler/setup'
 require 'ringo'
-require 'ringo/util/array_util'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
